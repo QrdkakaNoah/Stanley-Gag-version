20c377dcdbfd505108d556b102239470f0b6c54c
QrdkakaNoah <46796628+QrdkakaNoah@users.noreply.github.com>
2019-01-30 14:43:15 -0700

    Add files via upload

---
+++ Stanley (endless edition) final joke version.py
@@ -0,0 +1,88 @@
+###
+#Noah Gaviña
+#1.18.19
+#Employee #427 simulator endless final joke version
+###
+print("Credit to Davey Wreden for The Stanley Parable which this game is based on.")
+print("Game made by Masuhiro Sakurai.")
+print("Hello Employee #427.")
+while True:
+    y="8"
+    print("Press the " +y+ " key.")
+    x=input()
+    if x == y:
+        y="8"
+        print("Press the " +y+ " key.")
+        x=input()
+        if x == y:
+            y="8"
+            print("Press the " +y+ " key.")
+            x=input()
+            if x == y:
+                y="8"
+                print("Press the " +y+ " key.")
+                x=input()
+                if x == y:
+                    y="8"
+                    print("Press the " +y+ " key.")
+                    x=input()
+                    if x == y:
+                        y="8"
+                        print("Press the " +y+ " key.")
+                        x=input()
+                        if x == y:
+                            y="8"
+                            print("Press the " +y+ " key.")
+                            x=input()
+                            if x == y:
+                                y="8"
+                                print("Press the " +y+ " key.")
+                                x=input()
+                                if x == y:
+                                    y="8"
+                                    print("Press the " +y+ " key.")
+                                    x=input()
+                                    if x == y:
+                                        y="8"
+                                        print("Press the " +y+ " key.")
+                                        x=input()
+                                    else:
+                                        print("You are hired employee #427.")
+                                        print("Input anything to restart.")
+                                        input()
+                                else:
+                                    print("You are hired employee #427.")
+                                    print("Input anything to restart.")
+                                    input()
+                            else:
+                                print("You are hired employee #427.")
+                                print("Input anything to restart.")
+                                input()
+                        else:
+                            print("You are hired employee #427.")
+                            print("Input anything to restart.")
+                            input()
+                    else:
+                        print("You are hired employee #427.")
+                        print("Input anything to restart.")
+                        input()
+                else:
+                    print("You are hired employee #427.")
+                    print("Input anything to restart.")
+                    input()
+            else:
+                print("You are hired employee #427.")
+                print("Input anything to restart.")
+                input()
+        else:
+            print("You are hired employee #427.")
+            print("Input anything to restart.")
+            input()
+    else:
+        print("You are hired employee #427.")
+        print("Input anything to restart.")
+        input()
+else:
+    print("You are hired employee #427.")
+    print("Input anything to restart.")
+    input()
